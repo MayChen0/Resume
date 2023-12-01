e6dd36e1c14ed5639279963d9eaed76b7dc8f9d8
May <p36104136@gs.ncku.edu.tw>
2023-12-02 01:37:17 +0800

    build intro

--- src/component/intro/intro.jsx
+++ src/component/intro/intro.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ProfilePicture from "../../assets/image/profilePicture.jpg";
 import CV from "../../assets/document/CV.pdf";
+import { ReactComponent as DownloadIcon } from "../../assets/image/downloadIcon.svg";
 import "./intro.scss";
 const Intro = () => {
   return (
@@ -13,23 +14,26 @@ const Intro = () => {
         />
       </div>
       <div className="intro__text">
-        <h1>Hello,</h1>
-        <h1>I am May Chen</h1>
+        <h2>Hello,</h2>
+        <h2>I am May Chen</h2>
         <p>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad voluptas,
           ea illum illo aliquam neque aliquid magnam, odio impedit sunt
           consectetur a cumque fuga incidunt rem laborum, excepturi pariatur
           saepe.
         </p>
-        <div className="intro__text__cvDownloadLink">
-          <a
-            href={CV}
-            download="MayChenCV.pdf"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            CV
-          </a>
+        <div className="intro__text__cvDownloadContainer">
+          <div className="intro__text__cvDownloadLink">
+            <a
+              href={CV}
+              download="MayChenCV.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              &nbsp;&nbsp;C V
+            </a>
+            <DownloadIcon className="intro__text__cvDownloadLink__icon" />
+          </div>
         </div>
       </div>
     </div>
