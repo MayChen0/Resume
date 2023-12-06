d51565667562e809a5350aad436f36f23df1834f
May <p36104136@gs.ncku.edu.tw>
2023-12-07 03:58:55 +0800

    build work

--- src/component/work/work.jsx
+++ src/component/work/work.jsx
@@ -2,11 +2,67 @@ import React from "react";
 import "./work.scss";
 import WorkList from "./worklist";
 
+const WorkItem = ({ title, content, src, srcSet, classname }) => {
+  return (
+    <div className={`work__section__container__box ${classname} `}>
+      <h3 className="title">{title}</h3>
+      <img src={src} srcSet={srcSet} alt={title} />
+      <div className={`work__section__container__box__text `}>
+        <p className="content">{content}</p>
+        <a href="#">View Project</a>
+      </div>
+    </div>
+  );
+};
+
 const Work = () => {
+  const renderWorkItems = (category) => {
+    const items = WorkList.filter((item) => item.category === category);
+    return items.map((item, index) => (
+      <WorkItem
+        key={index}
+        classname={item.classname}
+        title={item.title}
+        content={item.content}
+        src={item.src}
+        srcSet={item.srcSet}
+        category={item.category}
+      />
+    ));
+  };
+
   return (
     <div className="work">
       <div className="work__title">
         <h1>MY WORK</h1>
+      </div>
+
+      <div className="work__section">
+        <h2>
+          PRODUCT
+          <br /> DESIGN
+        </h2>
+        <div className="work__section__container product">
+          {renderWorkItems("product")}
+        </div>
+        <h2>
+          UIUX
+          <br /> DESIGN
+        </h2>
+        <div className="work__section__container uiux">
+          {renderWorkItems("uiux")}
+        </div>
+        <h2>EXHIBITION</h2>
+        <div className="work__section__container exhibition">
+          {renderWorkItems("exhibition")}
+        </div>
+        <h2>
+          GRAPHIC
+          <br /> DESIGN
+        </h2>
+        <div className="work__section__container poster">
+          {renderWorkItems("poster")}
+        </div>
       </div>
     </div>
   );
--- src/component/work/worklist.js
+++ src/component/work/worklist.js
@@ -18,49 +18,58 @@ import WorkImage_Poster01_2x from '../../assets/image/poster1@2x.png'
 import WorkImage_Poster02_2x from '../../assets/image/poster2@2x.png'
 
 const WorkList = [
-    {
+    {   classname:"unicharm",
         title:"UNICHARM",
         content:"UNICHARM is a hairdressing set that help one-armed people dress up as their please, and exercise their right to be beautiful without being limited by their physical disability.",
         src: WorkImage_Unicharm,
         srcSet: `${WorkImage_Unicharm} 1x,${WorkImage_Unicharm_2x} 2x`,
+        category:"product",
     },
-    {
+    {   classname:"yichuan",
         title:"YI-CHUAN",
         content:"Yi-Chuan screen is a new kind of furniture that is created to slove the semi-dirty clothes problem. Blended with the Asian style, it is very suitable for Asian family.",
         src: WorkImage_Yichuan,
         srcSet: `${WorkImage_Yichuan} 1x,${WorkImage_Yichuan_2x} 2x`,
-    },{
+        category:"product",
+    },{ classname:"buddah",
         title:"BUDDAH",
         content:"Buddha key holder is the culture products that integrates Chinese Loyang elements, and Combined the casting and rusting techniques of the intangible cultural heritage of bronze.",
         src: WorkImage_Buddah,
         srcSet: `${WorkImage_Buddah} 1x,${WorkImage_Buddah_2x} 2x`,
-    },{
-        title:"PRINCE DORMITORY",
+        category:"product",
+    },{ classname:"dormitory",
+        title:"DORMITORY",
         content:"The design of Prince dormitory's check-in service process and repositioned the dormitory brand and vision. The service can reduce problems so that freshmen can enjoy their school life.",
         src: WorkImage_Dormitory,
         srcSet: `${WorkImage_Dormitory} 1x,${WorkImage_Dormitory_2x} 2x`,
-    },{
+        category:"uiux",
+    },{ classname:"whatif",
         title:"WHAT IF",
         src: WorkImage_Whatif,
         srcSet: `${WorkImage_Whatif} 1x,${WorkImage_Whatif_2x} 2x`,
-    },{
+        category:"exhibition",
+    },{ classname:"break",
         title:"BREAK THE DESIGN",
         src: WorkImage_Break,
         srcSet: `${WorkImage_Break} 1x,${WorkImage_Break_2x} 2x`,
-    },{
+        category:"exhibition",
+    },{ classname:"flipped",
         title:"FLIPPED THE DESIGN",
         src: WorkImage_Flipped,
         srcSet: `${WorkImage_Flipped} 1x,${WorkImage_Flipped_2x} 2x`,
+        category:"exhibition",
     },
-    {
+    {   classname:"poster_01",
         title:"POSTER DESIGN",
         src: WorkImage_Poster01,
         srcSet: `${WorkImage_Poster01} 1x,${WorkImage_Poster01_2x} 2x`,
+        category:"poster",
     },
-    {
+    {   classname:"poster-02",
         title:"POSTER DESIGN",
         src: WorkImage_Poster02,
         srcSet: `${WorkImage_Poster02} 1x,${WorkImage_Poster02_2x} 2x`,
+        category:"poster",
     },
 
 ];
