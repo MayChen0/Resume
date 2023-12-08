0845be67bfdc534f66be708de718d0992d24c414
May <p36104136@gs.ncku.edu.tw>
2023-12-09 04:14:27 +0800

    work RWD

--- src/component/work/work.jsx
+++ src/component/work/work.jsx
@@ -2,23 +2,36 @@ import React from "react";
 import "./work.scss";
 import WorkList from "./worklist";
 
-const WorkItem = ({ title, content, src, srcSet, classname }) => {
-  return (
-    <div className={`work__section__container__box ${classname} `}>
-      <h3 className="title">{title}</h3>
-      <img src={src} srcSet={srcSet} alt={title} />
-      <div className={`work__section__container__box__text `}>
-        <p className="content">{content}</p>
-        <a href="#">View Project</a>
-      </div>
+const categories = [
+  { key: "product", label: "PRODUCT DESIGN" },
+  { key: "uiux", label: "UI/UX DESIGN" },
+  { key: "exhibition", label: "EXHIBITION" },
+  { key: "poster", label: "GRAPHIC DESIGN" },
+];
+
+const WorkItem = ({ title, content, src, srcSet, classname }) => (
+  <div className={`work__section__container__box ${classname} `}>
+    <h3 className="title">{title}</h3>
+    <img src={src} srcSet={srcSet} alt={title} />
+    <div className={`work__section__container__box__text `}>
+      <p className="content">{content}</p>
+      <a href="#">View Project</a>
     </div>
-  );
-};
+  </div>
+);
 
 const Work = () => {
-  const renderWorkItems = (category) => {
-    const items = WorkList.filter((item) => item.category === category);
-    return items.map((item, index) => (
+  const renderWorkSection = (category) => (
+    <div
+      key={category.key}
+      className={`work__section__container ${category.key}`}
+    >
+      {renderWorkItems(category.key)}
+    </div>
+  );
+
+  const renderWorkItems = (category) =>
+    WorkList.filter((item) => item.category === category).map((item, index) => (
       <WorkItem
         key={index}
         classname={item.classname}
@@ -29,42 +42,27 @@ const Work = () => {
         category={item.category}
       />
     ));
-  };
 
   return (
     <div className="work">
       <div className="work__title">
         <h1>MY WORK</h1>
       </div>
-
       <div className="work__section">
-        <h2>
-          PRODUCT
-          <br /> DESIGN
-        </h2>
-        <div className="work__section__container product">
-          {renderWorkItems("product")}
-        </div>
-        <h2>
-          UIUX
-          <br /> DESIGN
-        </h2>
-        <div className="work__section__container uiux">
-          {renderWorkItems("uiux")}
-        </div>
-        <h2>EXHIBITION</h2>
-        <div className="work__section__container exhibition">
-          {renderWorkItems("exhibition")}
-        </div>
-        <h2>
-          GRAPHIC
-          <br /> DESIGN
-        </h2>
-        <div className="work__section__container poster">
-          {renderWorkItems("poster")}
-        </div>
+        {categories.map((category) => (
+          <>
+            <h2
+              key={`${category.key}-heading`}
+              className="work__section__categoryname"
+            >
+              {category.label}
+            </h2>
+            {renderWorkSection(category)}
+          </>
+        ))}
       </div>
     </div>
   );
 };
+
 export default Work;
