16de0bd8b1b9c61b0135e4d91685dfb943db6450
May <p36104136@gs.ncku.edu.tw>
2023-12-22 03:28:15 +0800

    add training

--- src/Pages/component/work/worklist.js
+++ src/Pages/component/work/worklist.js
@@ -2,6 +2,7 @@ import WorkImage_Unicharm from '../../../assets/image/unicharm.png'
 import WorkImage_Yichuan from '../../../assets/image/yichuan.png'
 import WorkImage_Buddah from '../../../assets/image/buddah.png'
 import WorkImage_Dormitory from '../../../assets/image/dormitory.png'
+import WorkImage_Training from '../../../assets/image/training.png'
 import WorkImage_Whatif from '../../../assets/image/whatif.png'
 import WorkImage_Break from '../../../assets/image/break.png'
 import WorkImage_Flipped from '../../../assets/image/flip.png'
@@ -10,7 +11,8 @@ import WorkImage_Poster02 from '../../../assets/image/poster2.png'
 import WorkImage_Unicharm_2x from '../../../assets/image/unicharm@2x.png'
 import WorkImage_Yichuan_2x from '../../../assets/image/yichuan@2x.png'
 import WorkImage_Buddah_2x from '../../../assets/image/buddah@2x.png'
-import WorkImage_Dormitory_2x from '../../../assets/image/dormitory.png'
+import WorkImage_Dormitory_2x from '../../../assets/image/dormitory@2x.png'
+import WorkImage_Training_2x from '../../../assets/image/training@2x.png'
 import WorkImage_Whatif_2x from '../../../assets/image/whatif@2x.png'
 import WorkImage_Break_2x from '../../../assets/image/break@2x.png'
 import WorkImage_Flipped_2x from '../../../assets/image/flip@2x.png'
@@ -24,7 +26,7 @@ const WorkList = [
         src: WorkImage_Unicharm,
         srcSet: `${WorkImage_Unicharm} 1x,${WorkImage_Unicharm_2x} 2x`,
         category:"product",
-        link: "/Unicharm"
+        link: "https://www.behance.net/gallery/186708811/Unicharm-Product-Design"
     },
     {   classname:"yichuan",
         title:"YI-CHUAN",
@@ -32,54 +34,61 @@ const WorkList = [
         src: WorkImage_Yichuan,
         srcSet: `${WorkImage_Yichuan} 1x,${WorkImage_Yichuan_2x} 2x`,
         category:"product",
-        link: "/Yichuan"
+        link: "https://www.behance.net/gallery/186707543/Yi-Chuan-Product-Design"
     },{ classname:"buddah",
         title:"BUDDAH",
         content:"Buddha key holder is the culture products that integrates Chinese Loyang elements, and Combined the casting and rusting techniques of the intangible cultural heritage of bronze.",
         src: WorkImage_Buddah,
         srcSet: `${WorkImage_Buddah} 1x,${WorkImage_Buddah_2x} 2x`,
         category:"product",
-        link: "/Buddah"
-    },{ classname:"dormitory",
-    
+        link: "https://www.behance.net/gallery/186728779/Buddah-Product-Design"
+    },{ classname:"dormitory",   
         title:"DORMITORY",
         content:"The design of Prince dormitory's check-in service process and repositioned the dormitory brand and vision. The service can reduce problems so that freshmen can enjoy their school life.",
         src: WorkImage_Dormitory,
         srcSet: `${WorkImage_Dormitory} 1x,${WorkImage_Dormitory_2x} 2x`,
         category:"uiux",
-        link: "/Dormitory"
-    },{ classname:"whatif",
+        link: "https://www.behance.net/gallery/186659875/Prince-Dormitory-UIUX-Design"
+    },{ classname:"training",   
+    title:"TRAINING GAME",
+    content:"Serious game for offshore wind turbine maintenance personnel training. To provide users with a good experience and achieve effective learning outcomes, we employed participatory design and agile design methodologies in the developing process.",
+    src: WorkImage_Training,
+    srcSet: `${WorkImage_Training} 1x,${WorkImage_Training_2x} 2x`,
+    category:"uiux",
+    link: "/Training"
+   }
+    ,{ classname:"whatif",
         title:"WHAT IF",
         src: WorkImage_Whatif,
         srcSet: `${WorkImage_Whatif} 1x,${WorkImage_Whatif_2x} 2x`,
         category:"exhibition",
-        link: "/Whatif"
+        link: "https://www.behance.net/gallery/186815187/What-if-Exhibition"
     },{ classname:"break",
         title:"BREAK THE DESIGN",
         src: WorkImage_Break,
         srcSet: `${WorkImage_Break} 1x,${WorkImage_Break_2x} 2x`,
         category:"exhibition",
-        link: "/Break"
+        link: "https://www.behance.net/gallery/186833771/Break-the-Design-Exhibition"
     },{ classname:"flipped",
         title:"FLIPPED THE DESIGN",
         src: WorkImage_Flipped,
         srcSet: `${WorkImage_Flipped} 1x,${WorkImage_Flipped_2x} 2x`,
         category:"exhibition",
-        link: "/Flipped"
+        link: "https://www.behance.net/gallery/186832395/Fipdded-Design-Exhibition"
     },
     {   classname:"poster_01",
         title:"POSTER DESIGN",
         src: WorkImage_Poster01,
         srcSet: `${WorkImage_Poster01} 1x,${WorkImage_Poster01_2x} 2x`,
         category:"poster",
-        link: "/Poster"
+        link: "https://www.behance.net/gallery/186648605/Poster-Design"
     },
     {   classname:"poster-02",
         title:"POSTER DESIGN",
         src: WorkImage_Poster02,
         srcSet: `${WorkImage_Poster02} 1x,${WorkImage_Poster02_2x} 2x`,
         category:"poster",
-        link: "/Poster"
+        link: ""
     },
 
 ];
---
+++ src/Pages/training/training.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./training.scss";
+
+const Training = () => {
+  return (
+    <div className="training">
+      <div className="training_container">
+        <h1>Offshore Wind Turbine Maintenance Training Game</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Training;
